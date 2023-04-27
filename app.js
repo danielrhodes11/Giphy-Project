c2b9d4e5b8e718a2b8624c27a287629709c2a13d
Daniel Rhodes <danielrhodes47@gmail.com>
2023-04-27 07:45:01 -0500

    adjust colum size on appended gif

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function addGif(res) {
     let numResults = res.data.length;
     if (numResults) {
       let randomIdx = Math.floor(Math.random() * numResults);
-      let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+      let $newCol = $("<div>", { class: "col-md-12 col-12 mb-4" });
       let $newGif = $("<img>", {
         src: res.data[randomIdx].images.original.url,
         class: "w-100"
